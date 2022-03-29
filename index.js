9efbb3c43fd224a6a3a4184a042a5ea03e2ba221
Ignacio Romanow <romanowignacio1@gmail.com>
2022-03-29 14:57:56 -0300

    add heroku

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use((request, response) => {
     error: 'not found'
   })
 })
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server runing on port ${PORT}`)
 })
